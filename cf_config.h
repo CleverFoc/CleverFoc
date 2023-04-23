92a9635d5538d360455b71d0c8eb7130dde0abf6
Yifan Xiao <1993996310@qq.com>
2023-04-23 21:37:47 +0800

    Remove cf_config.h from gitignore

---
+++ cf_config.h
@@ -0,0 +1,24 @@
+/**
+ * @file cf_config.h
+ * @author Yifan Xiao (1993996310@qq.com)
+ * @brief Configuration file. Remember to copy this file to the project root directory.
+ *  And **do not** include this file in your include path.
+ * @version 0.1
+ * @date 2023-04-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef CF_CONFIG_H
+#define CF_CONFIG_H
+
+/* Enable debug output */
+#define CF_CONFIG_DEBUG 1
+
+/* Define the putchar function */
+#define CF_CONFIG_PUTCHAR_FN(c) putchar(c)
+
+#define CF_CONFIG_OBJECT_NAME_LEN 16
+
+#endif
